641f16c484a27e2d7e3f4309f7167f2d63be28ae
AmTeamX <amteamxmail@gmail.com>
2026-01-25 16:32:46 +0700

    change choice

--- components/TestRunner/ChoiceQuestion.tsx
+++ components/TestRunner/ChoiceQuestion.tsx
@@ -18,7 +18,7 @@ export default function ChoiceQuestion({ question, onAnswer, selectedAnswer }: C
 
                 {question.image && (
                     <div className="mb-6">
-                        <div className="relative w-full h-125 rounded-2xl overflow-hidden border-4 border-gray-300">
+                        <div className="relative w-full h-125  overflow-hidden  ">
                             <Image
                                 src={question.image}
                                 alt={question.title}
@@ -45,11 +45,12 @@ export default function ChoiceQuestion({ question, onAnswer, selectedAnswer }: C
                             <button
                                 key={option.id}
                                 onClick={() => onAnswer(option.id)}
-                                className={`border-4 rounded-3xl p-6 transition-all hover:scale-105 active:scale-95 flex items-center gap-6 ${isSelected
-                                    ? 'bg-green-200 border-green-600 ring-4 ring-green-300'
-                                    : 'bg-blue-100 hover:bg-blue-200 border-blue-400'
-                                    }`}
+                                className={`  p-6 transition-all hover:scale-105 active:scale-95 flex items-center gap-6 `}
                             >
+                                <div className="text-3xl font-bold text-gray-800 text-left flex items-center gap-3">
+                                    {isSelected && <span className="text-green-600">✓</span>}
+                                    {option.id}. {option.label}
+                                </div>
                                 <div className="relative w-32 h-32 rounded-xl overflow-hidden shrink-0">
                                     <Image
                                         src={option.image}
@@ -60,10 +61,7 @@ export default function ChoiceQuestion({ question, onAnswer, selectedAnswer }: C
                                         unoptimized={option.image.startsWith('/')}
                                     />
                                 </div>
-                                <div className="text-3xl font-bold text-gray-800 text-left flex items-center gap-3">
-                                    {isSelected && <span className="text-green-600">✓</span>}
-                                    {option.id}. {option.label}
-                                </div>
+
                             </button>
                         );
                     })}
